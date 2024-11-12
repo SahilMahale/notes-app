e94ed8ee5664b5536fc12fb49cd13cab1145fbc6
SahilMahale <mahalesahil@gmail.com>
2024-11-12 19:46:09 +0530

    mockDb and notes UT

--- notes-backend/internal/notes/notes_test.go
+++ notes-backend/internal/notes/notes_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/SahilMahale/notes-backend/internal/db"
+	"github.com/SahilMahale/notes-backend/internal/db/mock"
 	"github.com/SahilMahale/notes-backend/internal/helper"
 	"github.com/SahilMahale/notes-backend/models"
 )
@@ -37,6 +38,40 @@ func TestNotesController_UpdateNote(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got1, tt.want1) {
 				t.Errorf("NotesController.UpdateNote() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func TestNotesController_CreateNote(t *testing.T) {
+	dbMock := mock.NewTestDB(t)
+	nCtrl := NewNoteController(dbMock)
+	type args struct {
+		title string
+		body  string
+	}
+	tests := []struct {
+		name        string
+		args        args
+		wantdNoteId string
+		wantMyerr   helper.MyHTTPErrors
+	}{
+		{
+			name: "CreateNote: Positive case",
+			args: args{
+				title: "Test Note",
+				body:  "Test body",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := nCtrl.CreateNote(tt.args.title, tt.args.body)
+			if got != tt.wantdNoteId {
+				t.Errorf("NotesController.CreateNote() got = %v, want %v", got, tt.wantdNoteId)
+			}
+			if !reflect.DeepEqual(got1, tt.wantMyerr) {
+				t.Errorf("NotesController.CreateNote() got1 = %v, want %v", got1, tt.wantMyerr)
 			}
 		})
 	}
