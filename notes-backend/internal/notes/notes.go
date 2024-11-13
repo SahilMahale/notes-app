211f3c616af11e2c2f8070ba3ed390811f96891d
SahilMahale <mahalesahil@gmail.com>
2024-11-13 15:01:30 +0530

    UTs Create Update Getall Notes

--- notes-backend/internal/notes/notes.go
+++ notes-backend/internal/notes/notes.go
@@ -32,7 +32,6 @@ func (b NotesController) CreateNote(title, body string) (string, helper.MyHTTPEr
 		Title:  title,
 		Body:   body,
 	}
-
 	if err := b.DbInterface.Db.Create(&note); err.Error != nil {
 		return "", helper.ErrorMatch(err.Error)
 	}
@@ -42,7 +41,6 @@ func (b NotesController) CreateNote(title, body string) (string, helper.MyHTTPEr
 }
 
 func (n NotesController) GetAllNotes() (models.NotesResp, helper.MyHTTPErrors) {
-
 	notesList := models.NotesResp{}
 	notes := []db.Note{}
 	res := n.DbInterface.Db.Find(&notes)
@@ -61,6 +59,7 @@ func (n NotesController) GetAllNotes() (models.NotesResp, helper.MyHTTPErrors) {
 		Err: nil,
 	}
 }
+
 func (n NotesController) DeleteNote(noteId string) helper.MyHTTPErrors {
 	note := db.Note{
 		NoteID: noteId,
@@ -92,6 +91,7 @@ func (n NotesController) UpdateNote(noteID string, note models.NotePatchRequest)
 			Err: nil,
 		}
 }
+
 func (n NotesController) GetNote(noteID string) (models.NoteResp, helper.MyHTTPErrors) {
 	note := db.Note{
 		NoteID: noteID,
--- notes-backend/internal/notes/notes_test.go
+++ notes-backend/internal/notes/notes_test.go
@@ -4,44 +4,10 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/SahilMahale/notes-backend/internal/db"
 	"github.com/SahilMahale/notes-backend/internal/db/mock"
 	"github.com/SahilMahale/notes-backend/internal/helper"
 	"github.com/SahilMahale/notes-backend/models"
 )
-
-func TestNotesController_UpdateNote(t *testing.T) {
-	type fields struct {
-		DbInterface db.DbConnection
-	}
-	type args struct {
-		noteID string
-		note   models.NotePatchRequest
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   models.NotePatchResp
-		want1  helper.MyHTTPErrors
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			n := NotesController{
-				DbInterface: tt.fields.DbInterface,
-			}
-			got, got1 := n.UpdateNote(tt.args.noteID, tt.args.note)
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NotesController.UpdateNote() got = %v, want %v", got, tt.want)
-			}
-			if !reflect.DeepEqual(got1, tt.want1) {
-				t.Errorf("NotesController.UpdateNote() got1 = %v, want %v", got1, tt.want1)
-			}
-		})
-	}
-}
 
 func TestNotesController_CreateNote(t *testing.T) {
 	dbMock := mock.NewTestDB(t)
@@ -51,10 +17,9 @@ func TestNotesController_CreateNote(t *testing.T) {
 		body  string
 	}
 	tests := []struct {
-		name        string
-		args        args
-		wantdNoteId string
-		wantMyerr   helper.MyHTTPErrors
+		name      string
+		args      args
+		wantMyerr helper.MyHTTPErrors
 	}{
 		{
 			name: "CreateNote: Positive case",
@@ -62,16 +27,112 @@ func TestNotesController_CreateNote(t *testing.T) {
 				title: "Test Note",
 				body:  "Test body",
 			},
+			wantMyerr: helper.MyHTTPErrors{
+				Err: nil,
+			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, got1 := nCtrl.CreateNote(tt.args.title, tt.args.body)
-			if got != tt.wantdNoteId {
-				t.Errorf("NotesController.CreateNote() got = %v, want %v", got, tt.wantdNoteId)
+			_, got := nCtrl.CreateNote(tt.args.title, tt.args.body)
+			if got.Err != tt.wantMyerr.Err {
+				t.Errorf("NotesController.CreateNote() gotMyERR = %v, wantMyErr %v", got.Err, tt.wantMyerr.Err)
 			}
-			if !reflect.DeepEqual(got1, tt.wantMyerr) {
-				t.Errorf("NotesController.CreateNote() got1 = %v, want %v", got1, tt.wantMyerr)
+		})
+	}
+}
+
+func TestNotesController_UpdateNote(t *testing.T) {
+	dbMock := mock.NewTestDB(t)
+	nCtrl := NewNoteController(dbMock)
+	noteID, err := nCtrl.CreateNote("Test Note", "Test Body")
+	if err.Err != nil {
+		t.Error(err.Err)
+	}
+	type args struct {
+		noteID string
+		note   models.NotePatchRequest
+	}
+	tests := []struct {
+		name      string
+		args      args
+		want      models.NotePatchResp
+		wantMyerr helper.MyHTTPErrors
+	}{
+		{
+			name: "UpdateNote: Positive Case",
+			args: args{
+				noteID: noteID,
+				note: models.NotePatchRequest{
+					Body: "Test Body updated",
+				},
+			},
+			want: models.NotePatchResp{
+				NoteID: noteID,
+				Title:  "Test Note",
+				Body:   "Test Body updated",
+			},
+			wantMyerr: helper.MyHTTPErrors{
+				Err: nil,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotMyErr := nCtrl.UpdateNote(tt.args.noteID, tt.args.note)
+			if got.Body != tt.want.Body && got.NoteID != tt.want.NoteID {
+				t.Errorf("NotesController.UpdateNote() got = %v, want %v", got, tt.want)
+			}
+			if gotMyErr.Err != tt.wantMyerr.Err {
+				t.Errorf("NotesController.UpdateNote() got1 = %v, want %v", gotMyErr, tt.wantMyerr)
+			}
+		})
+	}
+}
+
+func TestNotesController_GetAllNotes(t *testing.T) {
+	dbMock := mock.NewTestDB(t)
+	nCtrl := NewNoteController(dbMock)
+	noteID1, err := nCtrl.CreateNote("Test Note", "Test Body")
+	if err.Err != nil {
+		t.Error(err.Err)
+	}
+	noteID2, err := nCtrl.CreateNote("Test Note2", "Test Body2")
+	if err.Err != nil {
+		t.Error(err.Err)
+	}
+	tests := []struct {
+		want    models.NotesResp
+		wantErr helper.MyHTTPErrors
+		name    string
+	}{
+		{
+			name: "GetAllNotes: Positive",
+			want: models.NotesResp{
+				{
+					NoteID: noteID1,
+					Title:  "Test Note",
+					Body:   "Test Body",
+				},
+				{
+					NoteID: noteID2,
+					Title:  "Test Note2",
+					Body:   "Test Body2",
+				},
+			},
+			wantErr: helper.MyHTTPErrors{
+				Err: nil,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotErr := nCtrl.GetAllNotes()
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NotesController.GetAllNotes() got = %v, want %v", got, tt.want)
+			}
+			if gotErr.Err != tt.wantErr.Err {
+				t.Errorf("NotesController.GetAllNotes() got1 = %v, want %v", gotErr, tt.wantErr)
 			}
 		})
 	}
