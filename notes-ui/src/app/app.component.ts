e102c46fad5828117828a95600e59dfe6375a8b0
SahilMahale <mahalesahil@gmail.com>
2024-11-20 01:25:51 +0530

    basic UI + integartion of getNotes call
    Notes are fetched from backend and rendered, but JWT is hardcoded at run
    time
    TODO:
    - login page
    - authlayer in angular

--- notes-ui/src/app/app.component.ts
+++ notes-ui/src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './card/card.component';
+import { ApiService } from './api/api.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'notes-ui';
+  noteTitle = "Sahil's First note"
+  noteBody = "Note Body is ochinchin"
+  notes: any
+  constructor(private apiService: ApiService) { }
+  ngOnInit() {
+    this.fetchNotes()
+  }
+  fetchNotes() {
+    this.apiService.getNotes().subscribe(
+      {
+        next: (notes) => {
+          this.notes = notes
+        }
+      }
+    )
+  }
 }
